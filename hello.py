18a3934742f87e521dcc7ed90ba0e8259dc3171f
Joshua Park <kjoshuapark@gmail.com>
2021-09-26 20:09:54 -0400

    Added screenshot for activity 4 step 5

--- hello.py
+++ hello.py
@@ -39,6 +39,8 @@ def index():
         old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
         session['email'] = form.email.data
         return redirect(url_for('index'))
